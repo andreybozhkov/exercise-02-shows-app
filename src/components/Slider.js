4b28103e54715307ce1903bb90a60373da16558b
andreybozhkov <bozhkov.andrey@gmail.com>
2018-07-03 22:44:16 +0300

    ADDED custom component - more functionality later

---
+++ src/components/Slider.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Slider extends Component {
+    render() {
+        return (
+            <div>
+                <div>Prev</div>
+                <img alt='focusedEp' src='yourImgSrc'/>
+                <div>Next</div>
+            </div>
+        )
+    }
+}
+
+export default Slider;
