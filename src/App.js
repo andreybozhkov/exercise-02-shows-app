9c55311cc7d25e7088610de261734c834b6f05c0
andreybozhkov <bozhkov.andrey@gmail.com>
2018-07-04 21:49:39 +0300

    ADDED extend the main component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 import Slider from './components/Slider';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      epOnFocus : 0
+    };
+
+    this.changeEp = (ep) => {
+      this.setState({epOnFocus: ep});
+    }
+  }
+
   render() {
     return (
       <div className='App' >
-        <Slider />
+        <Slider updateFunc={this.changeEp} focusedEp={this.state.epOnFocus}/>
       </div>
     );
   }
